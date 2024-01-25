24dba0108b9b62983fa99b26350e140d1ad9e5c1
toda-watanabe <156382895+toda-watanabe@users.noreply.github.com>
2024-01-26 08:28:22 +0900

    住所からマップの座標の取得、表示

--- signage/js/iniad.js
+++ signage/js/iniad.js
@@ -6,6 +6,44 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 35.7804643, lng: 139.7151025 },
-        zoom: 15
+        zoom: 16
     });
 }
+
+function apiError(error) {
+    console.log('ERROR: ' + error);
+}
+
+function findlocation(){
+    // GoogleAPIから緯度・経度の取得
+    let inputAddress = document.getElementById("address");
+    let address = inputAddress.textContent;
+    fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURI(address) + '&key=' + API_KEY_GOOGLE)
+    .then(response => response.json())
+    .then(displaymap)
+    .catch(apiError);
+}
+
+function displaymap(result){
+    // error
+    if(result == null){
+        return;
+    }
+
+    let location = result.results[0].geometry.location;
+    let output_lat = document.getElementById("latitude");
+    let output_lon = document.getElementById("longitude");
+    let lat = location.lat;
+    let lon = location.lng;
+    output_lat.textContent = lat;
+    output_lon.textContent = lon;
+    map.setCenter({ lat: Number(lat), lng: Number(lon) });
+
+    // // ピンを立てる
+    // new google.maps.Marker({
+    //     position: center,
+    //     map: map
+    // });
+}
+
+let address = findlocation();
